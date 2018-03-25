4555a2f81b2639f66831d6719b7f1c454b9edc8e
João Lima <figorifico3000@gmail.com>
2018-03-25 13:02:26 +0100

    funções auxiliares

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "librarias.h"
+#include "auxiliares.h"
 
 int main(int argc, char const *argv[]) {
-  printf("ola\n");
+  char **guardar=malloc(sizeof(*guardar));
+  int quantidade;
+  quantidade=separarPor("ola eu sou ll"," ",&guardar);
+  mergeSortArrayString(&guardar,quantidade);
+  for(int i=0;i<quantidade;i++)
+  {
+    printf("%s\n",guardar[i]);
+  }
+  printf("%d\n",binarySearchArrayString(guardar,quantidade,"ola")+1);
   return 0;
 }
